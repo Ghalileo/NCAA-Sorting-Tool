4f34d0ff69cda8f5a1c2ede120f18518290ec76e
Promofinder2 <olivecaus@gmail.com>
2022-03-23 21:01:06 -0500

    ah, just push it

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import json
 import pandas as pd
 from decouple import config
 import csv
-
+import openpyxl
+from openpyxl.styles import Font, Color
 # API_key = config("THE_API_KEY")
 # url = 'https://api.sportsdata.io/v3/cbb/scores/json/Games/2022?key={API_key}'
 
@@ -23,19 +24,114 @@ s = open ('TX.json')
 cali = json.load(f)
 texas = json.load(s)
 
-<<<<<<< HEAD
-# Creates file to post response data 
-with open ('res.csv', 'w', newline="") as tex:
-# Creates headers for CSV file
-    fieldnames = ["TXHOME", "TXAWAY", "TXLOCATION", "UCLAHOME","UCLAAWAY","UCLALOCATION"]
-# Posts CSV file to header
-=======
 cali_df = pd.read_json('CA.json')
 tex_df = pd.read_json('TX.json')
 
+data_df = pd.read_csv('res.csv')
+
+tmp=[]
+counter=0
+for i,tx_home in enumerate(data_df['TXHOME'].to_list()):
+    print('new first loop',i,len(tmp))
+    if tx_home != 'TX':
+        j=0
+        for j,ucla in enumerate(data_df['UCLAAWAY'].to_list()):
+         #   print(j)
+            if tx_home==ucla:
+                counter+=1
+                print(f'inner append {counter}')
+                tmp.append(tx_home)
+                print('Found a match',tx_home, ucla,'Away Team index',j,'Home Team index',i)
+                break
+            elif j == len(data_df['UCLAAWAY'].to_list())-1:
+                tmp.append('')
+                print('no match')
+    else:
+        tmp.append('')
+workbook = openpyxl.load_workbook('test.xlsx')
+worksheet = workbook['Sheet1']
+tmp1=data_df['TXHOME_TO_UCLAAWAY_HITS'].to_list()
+tmp2=data_df['TXAWAY_TO_UCLAAWAY_HITS'].to_list()
+tmp3=data_df['TXAWAY_TO_UCLAHOME_HITS'].to_list()
+tmp4=data_df['TXHOME_TO_UCLAHOME_HITS'].to_list()
+cnt=0
+for a,b,c,d in zip(tmp1,tmp2,tmp3,tmp4):
+    if a != '':
+        col = 'A'
+        row = cnt+1
+        row = str(row)
+        val = a
+        placement=col+row
+        worksheet[placement]=val
+        worksheet[placement].font = Font(color='FF0000',bold=True)
+
+        
+    if b != '':
+        col = 'B'
+        row = cnt+1
+        row = str(row)
+        val = b
+        placement=col+row
+        worksheet[placement]=val
+        worksheet[placement].font = Font(color='FF0000',bold=True)
+
+        
+    if c != '':
+        col = 'C'
+        row = cnt+1
+        row = str(row)
+        val = c
+        placement=col+row
+        worksheet[placement]=val
+        worksheet[placement].font = Font(color='FF0000',bold=True)
+
+        
+    if d != '':
+        col='D'
+        row = cnt+1
+        row = str(row)
+        val = d
+        placement=col+row
+        worksheet[placement]=val
+        worksheet[placement].font = Font(color='FF0000',bold=True)
+    
+    cnt+=1
+
+workbook.save(filename='font_test.xlsx')      
+
+col = openpyxl.utils.get_column_letter('TXAWAY_TO_UCLAHOME_HITS')
+
+tmp=[]
+for row in worksheet['TXAWAY_TO_UCLAHOME_HITS']:
+    print(row)
+counter=0
+for i,tx_home in enumerate(data_df['TXHOME'].to_list()):
+    print('new first loop',i,len(tmp))
+    if tx_home != 'TX':
+        j=0
+        for j,ucla in enumerate(data_df['UCLAHOME'].to_list()):
+         #   print(j)
+            if tx_home==ucla:
+                counter+=1
+                print(f'inner append {counter}')
+                tmp.append(tx_home)
+                print('Found a match',tx_home, ucla,'Away Team index',j,'Home Team index',i)
+                break
+            elif j == len(data_df['UCLAAWAY'].to_list())-1:
+                tmp.append('')
+                print('no match')
+    else:
+        tmp.append('')
+
+data_df['TXHOME_TO_UCLAHOME_HITS']=tmp
+
+data_df.to_excel('test.xlsx',index=False)
+
+
+
+
 with open ('sample.csv', 'w', newline="") as tex:
     fieldnames = ["TXHOME", "TXAWAY","UCLAHOME","UCLAAWAY"]
->>>>>>> 2ca3ff18e85a594086769f2618f2c3f8a527ce31
     writer = csv.DictWriter(tex, fieldnames=fieldnames)
 # Initiates function
     writer.writeheader()
